4a47e0e103119a76c668223b658c1567f4be77d7
dishaa-sharmaa16 <disha11.inbox@gmail.com>
2025-07-16 12:15:12 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,271 @@
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+int main()
+{
+    // printf("hello, world\n");
+
+    /*int number = 20;
+    char star = '*';
+^    printf("%c", star);*/
+
+    /*
+
+    float number = 2.55;
+    printf("%f", number);*/
+
+    /*int age;
+     printf("enter age");
+     scanf("%d", &age);
+     printf("age is:%d", age);*/
+
+    /*int a =4+9*10;
+    printf("%d",a);*/
+
+    /*int a = 3, b = 6, c = 8;
+    printf("%d", a + b + c);*/
+
+    /*int a = 5 * 2 - 2 * 3;
+    int b = 5 * 2 / 2 * 3;
+    int c = 5 * (2 / 2) * 3;
+    int d = 5 + 2 / 2 * 3;
+    printf("%d \n", a);
+    printf("%d \n", b);
+    printf("%d \n", c);
+    printf("%d", d);*/
+
+    /*printf("%d \n", 4 == 4);
+    printf("%d \n", 4 >= 7);
+    printf("%d \n", 4 <= 7);
+    printf("%d \n", 4 != 7);
+    printf("%d \n", 4 > 3);
+    printf("%d \n", 4 > 8);*/
+
+    /*printf("%d \n", (4 > 2) && (5 > 2));
+    printf("%d \n", (4 < 7) || (5 > 4));
+    printf("%d \n", !(5 > 3)); */
+
+    /*int a;
+    printf("enter number");
+    scanf("%d", &a);
+
+    printf("%d", a % 2 == 0);*/
+
+    /*int x;
+    printf("enter number: ");
+    scanf("%d", &x);
+    printf("%d\n", x % 2 == 0);*/
+
+    /*int x;
+    int y;
+    int z;
+
+    printf("enter x: ");
+    scanf("%d", &x);
+    printf("enter y: ");
+    scanf("%d", &y);
+    printf("enter z: ");
+    scanf("%d", &z);
+
+    int avg = x + y + z / 2;
+    printf("%d", avg);*/
+
+    /*char ch;
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if (ch >= '0' && ch <= '9')
+    {
+        printf("The character '%c' is a digit.\n", ch);
+    }
+    else
+    {
+        printf("The character '%c' is NOT a digit.\n", ch);
+    }*/
+
+    /*int x;
+    int y;
+    int z;
+    printf("enter x: ");
+    scanf("%d", &x);
+    printf("enter y: ");
+    scanf("%d", &y);
+    printf("enter z: ");
+    scanf("%d", &z);
+
+    if (x <= y && x <= z)
+    {
+        printf("%d is the smallest number.\n", x);
+    }
+    else if (y <= x && y <= z)
+    {
+        printf("%d is the smallest number.\n", y);
+    }
+    else
+    {
+        printf("%d is the smallest number.\n", z);
+    }*/
+
+    /*int age;
+    printf("enter age : ");
+    scanf("%d", &age);
+
+    if (age > 18)
+    {
+        printf("adult");
+    }
+    else
+    {
+        printf("minor");
+    }*/
+
+    /*int marks;
+    printf("enter your marks: ");
+    scanf("%d", &marks);
+
+    if (marks > 90)
+    {
+        printf("A+");
+    }
+    else if (marks > 80 && marks < 90)
+    {
+        printf("A");
+    }
+    else
+    {
+        printf("hahaa");
+    }*/
+
+    /*int x;
+    int y;
+
+    printf("enter x: ");
+    scanf("%d", &x);
+    printf("enter y: ");
+    scanf("%d", &y);
+
+    if (x > y)
+    {
+        printf("the greater number is '%d'", x);
+    }
+    else
+    {
+        printf("the greater number is '%d'", y);
+    }*/
+
+    /*int marks;
+    printf("enter your marks: ");
+    scanf("%d", &marks);
+
+    if (marks >= 90)
+    {
+        printf("GRADE A");
+    }
+    else if (marks >= 80 && marks < 90)
+    {
+        printf("GRADE B");
+    }
+    else if (marks >= 70 && marks < 80)
+    {
+        printf("GRADE C");
+    }
+    else if (marks >= 60 && marks < 70)
+    {
+        printf("GRADE D");
+    }
+    else if (marks >= 50 && marks < 60)
+    {
+        printf("GRADE E");
+    }
+    else
+    {
+        printf("GRADE F");
+    }*/
+
+    /*int x, y;
+    char operation;
+
+    printf("Enter x: ");
+    scanf("%d", &x);
+
+    printf("Enter y: ");
+    scanf("%d", &y);
+
+    printf("Enter operation (+, -, *, /): ");
+    scanf(" %c", &operation); // Notice the space before %c
+
+    if (operation == '+')
+    {
+        printf("The answer is %d\n", x + y);
+    }
+    else if (operation == '-')
+    {
+        printf("The answer is %d\n", x - y);
+    }
+    else if (operation == '*')
+    {
+        printf("The answer is %d\n", x * y);
+    }
+    else if (operation == '/')
+    {
+        if (y != 0)
+            printf("The answer is %d\n", x / y);
+        else
+            printf("Division by zero is not allowed.\n");
+    }
+    else
+    {
+        printf("Invalid operation.\n");
+    }*/
+
+    /*int num;
+    printf("enter number: ");
+    scanf("%d", &num);
+
+    if (num == 1)
+    {
+        printf("1- ONE");
+    }
+    else if (num == 2)
+    {
+        printf("2- TWO");
+    }
+    else if (num == 3)
+    {
+        printf("3- THREE");
+    }
+    else if (num == 4)
+    {
+        printf("4- FOUR");
+    }
+    else if (num == 5)
+    {
+        printf("5- FIVE");
+    }
+    else if (num == 6)
+    {
+        printf("6- SIX");
+    }
+    else if (num == 7)
+    {
+        printf("7- SEVEN");
+    }
+    else if (num == 8)
+    {
+        printf("8- EIGHT");
+    }
+    else if (num == 9)
+    {
+        printf("9- NINE");
+    }
+    else if (num == 10)
+    {
+        printf("10- TEN");
+    }
+    else
+    {
+        printf("invalid output");
+    }*/
+
+    return 0;
+}
